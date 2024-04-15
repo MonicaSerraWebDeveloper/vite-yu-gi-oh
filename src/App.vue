a6e19f46e3162eb04b8b420179cc00451873ccf8
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-15 16:08:30 +0200

    feat: changed the params from the string of the url to a variable called 'queryParams'

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,15 @@
 
     methods: {
       cardsFromApi() {
-        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+
+        const queryParams = {
+          num: 20,
+          offset: 0
+        };
+
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php', {
+          params: queryParams
+        })
         .then((response) => {
           store.cards = response.data.data
         })
