c79e0951e67f3c79c37dea1df6440507b3109404
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-15 14:36:58 +0200

    feat: add the main section to prepare the container of the grid cards

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
   import AppHeader from './components/AppHeader.vue'
+  import AppGrid from './components/AppGrid.vue'
 
   export default {
     components: {
       AppHeader,
+      AppGrid
     }
   }
 
@@ -12,10 +14,13 @@
 <template>
 
   <AppHeader></AppHeader>
+  <main>
+    <AppGrid></AppGrid>
+  </main>
 
 </template>
 
 <style lang="scss">
-  @use './style/generic'
-  
+  @use './style/generic';
+
 </style>
---
+++ src/components/AppGrid.vue
@@ -0,0 +1,29 @@
+<script>
+
+    export default {
+        name: 'AppGrid'
+    }
+
+</script>
+
+<template>
+    <div class="bg-container">
+        <div class="container">
+            <div class="row">
+                <h2>Grid</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .bg-container {
+        background-color: sandybrown;
+        padding: 50px 0;
+
+        .container {
+            background-color: #fff;
+            padding: 30px;
+        }
+    }
+</style>
