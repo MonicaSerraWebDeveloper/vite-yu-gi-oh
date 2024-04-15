3f6ce70fa0fc66bbdc0fd6ed176e20b9d5471c00
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-15 14:27:23 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+
+    export default {
+        name: 'AppHeader'
+    }
+
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <h1>Yu-Gi-Oh Api</h1>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+    header {
+        background-color: #fff;
+
+        h1 {
+            font-weight: 300;
+            padding: 10px 0;
+        }
+    }
+
+</style>
